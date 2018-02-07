098c13dadaba2fe2c7ff975efbf6f78dafa1baf9
Dmitry Simonov <demalf@gmail.com>
2018-02-08 00:23:15 +0300

    tickers added

---
+++ fabfile.py
@@ -0,0 +1,29 @@
+from fabric.api import local, task
+
+@task
+def clean():
+    local("rm -rf .eggs build cryptoportfolio.egg-info dist .mypy_cache")
+
+@task
+def test():
+    local("flake8 .")
+    local("mypy --ignore-missing-imports .")
+    local("python3 setup.py test")
+
+
+@task
+def install():
+    local("python3 setup.py install")
+
+
+@task
+def dist():
+    local("python3 setup.py sdist")
+
+
+@task
+def upload():
+    dist()
+    version = local("python3 setup.py -V", capture=True).stdout.strip()
+    local("git tag %s" % version)
+    local("twine upload dist/*")
